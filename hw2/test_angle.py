d12161cc482a7f390375915cc2314f34dd4e2d50
Sergii Zubovych <ext-sergii.zubovych@here.com>
2021-02-12 23:02:33 +0200

    Additional oriented angle tests

--- hw2/test_angle.py
+++ hw2/test_angle.py
@@ -5,16 +5,23 @@ import numpy as np
 def test_oriented_сw():
     assert angle.oriented(Segment(Point(0, 0), Point(2, 2)), Segment(Point(0, 0), Point(-3, 3))) == np.radians(90)
     assert angle.oriented(Segment(Point(0, 0), Point(-3, -3)), Segment(Point(0, 0), Point(2, -2))) == np.radians(90)
-    assert angle.oriented(Segment(Point(0, 0), Point(2, 2)), Segment(Point(0, 0), Point(3, 3))) == 0
+    assert angle.oriented(Segment(Point(0, 0), Point(2, 2)), Segment(Point(0, 0), Point(-3, 0))) == np.radians(135)
+    assert angle.oriented(Segment(Point(0, 0), Point(-3, -3)), Segment(Point(0, 0), Point(2, 0))) == np.radians(135)
 
 
 def test_oriented_ccw():
     assert angle.oriented(Segment(Point(0, 0), Point(-3, 3)), Segment(Point(0, 0), Point(2, 2))) == -np.radians(90)
     assert angle.oriented(Segment(Point(0, 0), Point(2, -2)), Segment(Point(0, 0), Point(-3, -3))) == -np.radians(90)
-    assert angle.oriented(Segment(Point(0, 0), Point(3, 3)), Segment(Point(0, 0), Point(2, 2))) == 0
+    assert angle.oriented(Segment(Point(0, 0), Point(-3, 0)), Segment(Point(0, 0), Point(2, 2))) == -np.radians(135)
+    assert angle.oriented(Segment(Point(0, 0), Point(2, 0)), Segment(Point(0, 0), Point(-3, -3))) == -np.radians(135)
 
 
 def test_pi():
     assert angle.oriented(Segment(Point(0, 0), Point(-3, -3)), Segment(Point(0, 0), Point(2, 2))) == np.pi
     assert angle.oriented(Segment(Point(0, 0), Point(2, 2)), Segment(Point(0, 0), Point(-3, -3))) == np.pi
+
+
+def test_zero():
+    assert angle.oriented(Segment(Point(0, 0), Point(2, 2)), Segment(Point(0, 0), Point(3, 3))) == 0
+    assert angle.oriented(Segment(Point(0, 0), Point(3, 3)), Segment(Point(0, 0), Point(2, 2))) == 0
 
